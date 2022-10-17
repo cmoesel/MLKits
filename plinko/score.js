fb0a5ef837077fd7923e6aceffa1c711d5a52056
Chris Moesel <cmoesel@mitre.org>
2022-10-17 17:51:17 -0400

    Through lecture 24

--- plinko/score.js
+++ plinko/score.js
@@ -1,14 +1,28 @@
 const outputs = [];
-const predictionPoint = 300;
-const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  const bucket = _.chain(outputs)
-    .map(row => [distance(row[0]), row[3]])
+  const testSetSize = 100;
+  const [testSet, trainingSet] = splitDataSet(outputs, testSetSize);
+
+  _.range(1, 20).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log('For k of', k, 'accuracy is:', accuracy);
+  })
+}
+
+
+function knn(data, point, k) {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -18,12 +32,10 @@ function runAnalysis() {
     .first()
     .parseInt()
     .value();
-
-  console.log('Your point will probably fall into', bucket);
 }
 
-function distance(point) {
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB) {
+  return Math.abs(pointA - pointB);
 }
 
 function splitDataSet(data, testCount) {
